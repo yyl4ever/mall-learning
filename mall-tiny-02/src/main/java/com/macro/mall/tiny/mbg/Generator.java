da5108e6dd8429bc6a2fd916d6c9383e54ed0ea3
yyl4ever <yyl4ever@foxmail.com>
2019-08-23 15:30:12 +0800

    mall-tiny-02 集成 Swagger

---
+++ mall-tiny-02/src/main/java/com/macro/mall/tiny/mbg/Generator.java
@@ -0,0 +1,39 @@
+package com.macro.mall.tiny.mbg;
+
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.internal.DefaultShellCallback;
+
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * mbg 代码生成器，运行即可生成代码
+ * @Author: yangyl
+ * @Date: 2019/8/22 14:20
+ */
+public class Generator {
+    public static void main(String[] args) throws Exception {
+        // mbg 执行过程中的警告信息
+        List<String> warnings = new ArrayList<>();
+        // 当生成的代码重复时，覆盖原代码
+        boolean overwrite = true;
+        // 读取 mbg 配置文件
+        InputStream is = Generator.class.getResourceAsStream("/generatorConfig.xml");
+        ConfigurationParser cp = new ConfigurationParser(warnings);
+        Configuration config = cp.parseConfiguration(is);
+        is.close();
+
+        DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+        // 创建 mbg
+        MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
+        // 执行生产代码
+        myBatisGenerator.generate(null);
+        // 输出警告信息
+        for (String warning : warnings) {
+            System.out.println(warning);
+        }
+    }
+}
